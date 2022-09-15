d2a9de483708f08af4e92e62a4885b9bad6b0433
Juan Salazar <jsalazarv8@gmail.com>
2022-09-14 19:32:25 -0500

    Added bottom banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,23 @@
 import './styles.scss';
-import bannerTop from '../../assets/images/home/banner/banner.webp';
 import { Button } from '../../components/Button/index.jsx';
 import { Banner } from '../../components/Banner/index.jsx';
+import bannerTop from '../../assets/images/home/banner/banner.webp';
+import bannerBottom from '../../assets/images/home/banner/banner-bottom.webp';
 
 export const Home = () => {
   return (
     <div className="home">
       <Banner className="banner-top" image={bannerTop}>
-        <h1 className="banner-top__title">EL MEJOR DEPORTE</h1>
+        <h1 className="home__title">EL MEJOR DEPORTE</h1>
         <p className="banner-top__paragraph">
           Lorem Ipsum is simply dummy text of the <br /> printing and
           typesetting industry.
         </p>
-        <Button className="banner-top__button">Comprar ahora</Button>
+        <Button className="home__button">Comprar ahora</Button>
+      </Banner>
+      <Banner className="banner-bottom" image={bannerBottom}>
+        <h1 className="home__title">DESCUBRE TU PASIÓN</h1>
+        <Button className="home__button">Comprar ahora</Button>
       </Banner>
     </div>
   );
