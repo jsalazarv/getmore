04a0570a196b48abb8d499378fee2fb411ccf997
Juan Salazar <jsalazarv8@gmail.com>
2022-09-16 14:58:02 -0500

    Added Best Sellers section

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,20 @@
 import './styles.scss';
 
-export const Card = ({ image, title, className }) => {
+export const Card = ({
+  image,
+  title,
+  description = '',
+  price = '',
+  className,
+}) => {
   return (
     <div className="card">
-      <img className="card-image" src={image} alt="" />
+      <div className="card-image-container">
+        <img className="card-image" src={image} alt="" />
+      </div>
       <h3 className="card-title">{title}</h3>
+      <p className="card-description">{description}</p>
+      <span className="card-price">{price}</span>
     </div>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,38 @@
 import './styles.scss';
 import { Button } from '../../components/Button/index.jsx';
 import { Banner } from '../../components/Banner/index.jsx';
+import { Card } from '../../components/Card';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import bannerTop from '../../assets/images/home/banner/banner.webp';
 import bannerBottom from '../../assets/images/home/banner/banner-bottom.webp';
 import rackets from '../../assets/images/home/Rackets.webp';
 import balls from '../../assets/images/home/Balls.webp';
 import accessories from '../../assets/images/home/Accessories.webp';
-import { Card } from '../../components/Card';
+import product1 from '../../assets/images/home/products/product1.webp';
+import product2 from '../../assets/images/home/products/product2.webp';
+import product3 from '../../assets/images/home/products/product3.webp';
 
 export const Home = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  };
   return (
     <div className="home">
       <Banner className="banner-top" image={bannerTop}>
@@ -19,6 +43,33 @@ export const Home = () => {
         </p>
         <Button className="home__button">Comprar ahora</Button>
       </Banner>
+      <div className="best-sellers">
+        <div className="best-sellers-products">
+          <Slider {...settings}>
+            <Card
+              image={product1}
+              description="Wilson Championship Extra Duty Tennis Balls, 3-pk"
+              price="$250.00"
+            />
+            <Card
+              image={product2}
+              description="Wilson Championship Extra Duty Tennis Balls, 3-pk"
+              price="$250.00"
+            />
+            <Card
+              image={product3}
+              description="Wilson Championship Extra Duty Tennis Balls, 3-pk"
+              price="$250.00"
+            />
+            <Card
+              image={product3}
+              description="Wilson Championship Extra Duty Tennis Balls, 3-pk"
+              price="$250.00"
+            />
+          </Slider>
+        </div>
+        <Button className="home__button">Ver todos</Button>
+      </div>
       <div className="categories">
         <Card image={rackets} title="RAQUETAS" />
         <Card image={balls} title="PELOTAS" />
